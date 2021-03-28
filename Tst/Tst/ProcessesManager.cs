2d76a906d803bec3818728a46ea8ebdd4d2dbe89
qlulp <53346815+qlulp@users.noreply.github.com>
2021-03-28 06:03:51 +0300

    Add files via upload

---
+++ Tst/Tst/ProcessesManager.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tst
+{
+    class ProcessesManager
+    {
+
+        /// <summary>
+        /// Удаление процессов
+        /// </summary>
+        /// <param name="processes">массив процессов для удаления</param>
+        public static void KillProcesses(in Process[] processes)
+        {
+            foreach (Process process in processes)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($"Попытка удалить процесс {process.ProcessName}...\t");
+                try
+                {
+                    process.Kill();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Процесс успешно удалён");
+                    Console.ResetColor();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Вывод процессов
+        /// </summary>
+        /// <param name="processes">массив процессов для вывода</param>
+        public static void Output(in Process[] processes)
+        {
+            int i = GetMax(in processes) + 4;
+            foreach (Process process in processes)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write($"{process.ProcessName}");
+                for (int j = process.ProcessName.Length; j < i; j++)
+                    Console.Write(" ");
+                Console.ResetColor();
+                Console.WriteLine($"{process.PagedMemorySize64 / 1024}КБ");
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Получить максимальную длину названия процесса
+        /// </summary>
+        /// <param name="processes">массив процессов</param>
+        /// <returns></returns>
+        private static int GetMax(in Process[] processes)
+        {
+            int i = processes[0].ProcessName.Length;
+
+            for (int j = 0; j < processes.Length; j++)
+            {
+                if (i < processes[j].ProcessName.Length)
+                    i = processes[j].ProcessName.Length;
+            }
+            return i;
+        }
+
+
+        /// <summary>
+        /// Поиск процессов по ключевому слову
+        /// </summary>
+        /// <param name="processes">Массив процессов</param>
+        /// <param name="key">Ключевое слово, содержащееся в названии процесса</param>
+        /// <returns>Массив процессов, элементы которого удовлетворяют условиям поиска</returns>
+        public static Process[] Find(in Process[] processes, string key)
+        {
+            return processes.Where(i => i.ProcessName.Contains(key)).ToArray();
+        }
+
+    }
+}
---
+++ Tst/Tst/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Tst
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+            Process[] processes =  Process.GetProcesses();
+            
+            ProcessesManager.Output(processes);
+            Console.Write("Удалить процессы, содержащие строку в названии: ");
+            processes = ProcessesManager.Find(in processes, Console.ReadLine());
+
+            if (processes.Length > 0)
+            {
+                Console.WriteLine($"\nНайдены следующие процессы:");
+                Console.ForegroundColor = ConsoleColor.White;
+                ProcessesManager.Output(processes);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.WriteLine($"Всего найдено: {processes.Length}");
+                Console.ResetColor();
+                Console.Write("Удалить? y/n: ");
+
+                if (Console.ReadLine().ToLower() == "y")
+                {
+                    ProcessesManager.KillProcesses(in processes);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ничего не найдено");
+            }
+            Console.ReadKey();
+            Main();
+        }
+    }
+}
